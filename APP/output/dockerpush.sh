bd3a20c261402918154cfa37e2aeaf4df325cd88
77yashwanth@gmail.com <77yashwanth@gmail.com>
2024-12-04 03:47:23 +0530

    updating the workflows

--- APP/output/dockerpush.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 061051251404.dkr.ecr.us-east-1.amazonaws.com
-
-docker build -t output .
-docker tag output:latest 061051251404.dkr.ecr.us-east-1.amazonaws.com/stockdata/:latest
-docker push 061051251404.dkr.ecr.us-east-1.amazonaws.com/stockdata/:latest
